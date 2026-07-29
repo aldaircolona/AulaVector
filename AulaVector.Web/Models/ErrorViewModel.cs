dfb9ad945fd4ca21b257bcc833c40f098c106293
nebadigital <aldaircolona22@gmail.com>
2026-07-29 17:15:27 -0500

    chore: create project and solution

---
+++ AulaVector.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AulaVector.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
