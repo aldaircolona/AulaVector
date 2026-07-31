f5d7d76c1496fdfd0d1b36130c489891fa184083
nebadigital <aldaircolona22@gmail.com>
2026-07-30 19:59:32 -0500

    feat(web): dbinitializer if role does not exists

--- AulaVector.Data/Migrations/20260731003829_RenameUserProperties.Designer.cs
+++ AulaVector.Data/Migrations/20260731003829_RenameUserProperties.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AulaVector.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AulaVector.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260731003829_RenameUserProperties")]
+    partial class RenameUserProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AulaVector.Data/Migrations/20260731003829_RenameUserProperties.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AulaVector.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUserProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nombres",
+                table: "AspNetUsers",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "FechaRegistro",
+                table: "AspNetUsers",
+                newName: "RegistrationDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Estado",
+                table: "AspNetUsers",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "Dirección",
+                table: "AspNetUsers",
+                newName: "Country");
+
+            migrationBuilder.RenameColumn(
+                name: "Apellidos",
+                table: "AspNetUsers",
+                newName: "FirstName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegistrationDate",
+                table: "AspNetUsers",
+                newName: "FechaRegistro");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "AspNetUsers",
+                newName: "Nombres");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "AspNetUsers",
+                newName: "Estado");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "AspNetUsers",
+                newName: "Apellidos");
+
+            migrationBuilder.RenameColumn(
+                name: "Country",
+                table: "AspNetUsers",
+                newName: "Dirección");
+        }
+    }
+}
