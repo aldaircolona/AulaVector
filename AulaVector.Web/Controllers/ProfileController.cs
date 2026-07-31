f123e999aab0ae85571b5c3ba6206b88b48d0e07
nebadigital <aldaircolona22@gmail.com>
2026-07-31 18:36:26 -0500

    feat(profile): create user profile details

---
+++ AulaVector.Web/Controllers/ProfileController.cs
@@ -0,0 +1,31 @@
+using AulaVector.Models;
+using AulaVector.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AulaVector.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+    }
+}
